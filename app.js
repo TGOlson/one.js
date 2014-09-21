97db306baf5557e2e35b72ca8107e4ba4962e8bd
tgolson <tydotg@gmail.com>
2014-09-20 18:49:07 -0700

    Match class names.

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
 // main application scripts
 // one.js consuming code here
 
-$(init);
+$(function() {
 
-function init() {
-
-  One.initDOM({
-    h1: 'Hi there',
-    p: 'Some sub-text',
+One.HTML.init({
+  h1: 'Hi there',
+  p: 'Some sub-text',
+  div: {
+    h3: 'Headline in a div',
+    p: 'Text in a div',
     div: {
-      h3: 'Headline in a div',
-      p: 'Text in a div',
-      div: {
-        p: [
-          'Nested text in a div',
-          'Another paragraph in the same div'
-        ]
-      }
-    },
-    ul: {
-      li: ['item one', 'item two']
+      p: [
+        'Nested text in a div',
+        'Another paragraph in the same div'
+      ]
     }
-  });
+  },
+  ul: {
+    li: ['item one', 'item two']
+  }
+});
 
-  // One.style({
-  //   h1: {
-  //     color: 'red',
-  //     padding: '5px'
-  //   },
-  //   li: {
-  //     list-style: none;
-  //   }
-  // });
+// One.style({
+//   h1: {
+//     color: 'red',
+//     padding: '5px'
+//   },
+//   li: {
+//     list-style: none;
+//   }
+// });
 
-}
+
+});
--- one.js
+++ one.js
@@ -4,53 +4,95 @@
   One = definition();
 
 })(function() {
-  "use strict";
+"use strict";
 
-  var One = function() {};
+    // main module
+var One  = {},
 
-  One.initDOM = function(domObject) {
-    var elements = $.map(domObject, evaluateElementObject),
-      body = $('body');
+    // html sub-module
+    HTML = {},
 
-    append(body, elements);
-  };
+    // css sub-module
+    CSS  = {};
 
-  function evaluateElementObject(value, elementName) {
-    var element;
 
-    if(typeof value === 'string') {
-      element = createElementWithText(elementName, value);
-    }
+HTML.init = function(domObject) {
+  var elements = $.map(domObject, evaluateElementObject),
+    body = $('body');
 
-    if(typeof value === 'object') {
-      if(Array.isArray(value)) {
-        element = $.map(value, function(value) {
-          console.log(elementName, value);
-          return evaluateElementObject(value, elementName);
-        });
-      } else {
-        var elements = $.map(value, evaluateElementObject);
-        element = createElement(elementName).append(elements);
-      }
-    }
+  append(body, elements);
+};
 
-    return element;
+function evaluateElementObject(value, elementName) {
+  var element;
+
+  if(typeof value === 'string') {
+    element = createElementWithText(elementName, value);
   }
 
-  function createElement(elementName) {
-    return $('<' + elementName + '>');
+  if(Array.isArray(value)) {
+    element = $.map(value, function(value) {
+      return evaluateElementObject(value, elementName);
+    });
+  } else if(typeof value === 'object') {
+    var elements = $.map(value, evaluateElementObject);
+    element = createElement(elementName).append(elements);
   }
 
-  function createElementWithText(elementName, text) {
-    var element = createElement(elementName)
-      .text(text);
+  return element;
+}
 
-    return element;
+function createElement(elementName) {
+  return $('<' + elementName + '>');
+}
+
+function getClass(elementName) {
+  var match = elementName.match(/[.](\w+)(-?)(\w+)/g),
+    klass;
+
+  if(match) {
+
+    // take second element of match, which does not include preceding '.'
+    klass = match; // [1].split('.');
   }
 
-  function append(parent, element) {
-    parent.append(element);
+  return klass;
+}
+
+window.getClass = getClass;
+window.getId = getId;
+
+// only match 1 id per element
+function getId(elementName) {
+  // http://stackoverflow.com/questions/192048/can-an-html-element-have-multiple-ids
+  var match = elementName.match(/[#](\S+)(?=(.|$))/),
+    id;
+
+  if(match) {
+
+    // take second element of match, which does not include preceding '.'
+    id = match[1].split('.');
   }
 
-  return One;
+  return id;
+}
+
+
+function createElementWithText(elementName, text) {
+  var element = createElement(elementName)
+    .text(text);
+
+  return element;
+}
+
+function append(parent, element) {
+  parent.append(element);
+}
+
+// attach sub-modules
+One.HTML = HTML;
+One.CSS = CSS;
+
+return One;
+
 });
